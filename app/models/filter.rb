820135a9730afa9372d918cde5abcd1b48fc9e7a
Zhi Pan <zhipan@berkeley.edu>
2016-03-16 11:30:07 -0700

    added user api

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,4 +1,8 @@
 class Filter < Sequel::Model
   plugin :class_table_inheritance, key: :filter_type
   many_to_one :user
+
+  def filter(listings)
+    listings
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'homes#index'
   get '/filter', to: 'homes#filtered_listings'
+  get '/users/:id/listings', to: 'user#get_listings'
 end
