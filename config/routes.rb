ce78a6d7572026c1a3e49ffab5679cef7b682799
Zhi Pan <zhipan@berkeley.edu>
2016-03-24 21:47:32 -0500

    added set filter endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/users/:id/listings', to: 'user#get_listings'
   get '/fuck', to: 'agent#index'
   patch '/users/:id/update', to: 'user#update'
+  post '/filter/set', to: 'filters#set_filter'
 end
